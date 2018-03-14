6be4d9b11aa87765b04269d6dfb35deaf236cbf2
Deylon <deyloncarlo@gmail.com>
2018-03-14 00:46:50 -0300

    coletando conteúdo dos links

--- linkColector.py
+++ linkColector.py
@@ -13,7 +13,7 @@ class LinkCrawler(scrapy.Spider):
 
         def getLinksCrawled():
             v_linksColleted = []
-            v_file = open('/home/deylon/Documentos/links/linksCrawled.txt', 'r')
+            v_file = open('links/linksCrawled.txt', 'r')
             for v_line in v_file:
                 v_linksColleted.append(v_line);
             
@@ -24,7 +24,7 @@ class LinkCrawler(scrapy.Spider):
         v_links = []
         v_linksColleted = getLinksCrawled()
         print("\nLendo arquivo de links...\n")
-        v_file = open('/home/deylon/Documentos/links/links.txt', 'r')
+        v_file = open('links/links.txt', 'r')
         for v_line in v_file:
             if((v_line in v_linksColleted) == False):
                 v_line = v_line.replace('\n', '')
@@ -41,8 +41,13 @@ class LinkCrawler(scrapy.Spider):
         def contentCrawler(self, response):
             print('Crawler Start')
             v_titulo = response.xpath('//h1[@class="content-head__title"]/text()').extract_first()
-            print('H1?: ', h1)
-
+            print('Titulo: ', v_titulo)
+            v_autor = response.xpath('//p[@class="content-publication-data__from"]/text()').extract_first()
+            print('Autor: ', v_autor)
+            v_dataPublicacao = response.xpath('//time[@itemprop="datePublished"]/text()').extract_first()
+            print('Data: ', v_dataPublicacao)
+            v_conteudo = response.xpath('//p[contains(@class,"content-text__container")]/text()').extract()
+            print('Conteudo: ', v_conteudo)
             #for v_div in response.css('div.feed-text-wrapper'):
             #    v_link = v_div.css('a').xpath('@href').extract_first()
             #    v_link = v_link.encode('ascii', 'ignore')
